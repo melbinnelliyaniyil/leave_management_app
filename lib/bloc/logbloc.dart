5ab6dc72bd501cec9be7851da11d4433a67d6da6
melbinmathew <melbinnelliyaniyil107@gmail.com>
2022-07-27 20:44:32 +0530

    kkk

---
+++ lib/bloc/logbloc.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:leave_management_app/data/models/loginmodel.dart';
+import 'package:leave_management_app/data/repository.dart';
+import 'package:leave_management_app/helper/tempstorage.dart';
+
+
+
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthState()) {
+
+    on<CheckOTP>(_checkOtp);
+  }
+
+
+
+  Future<FutureOr<void>> _checkOtp(
+      CheckOTP event, Emitter<AuthState> emit) async {
+    emit(CheckingOtp());
+    LoginModel loginModel;
+    Map body = {
+      "email": event.phone.toString(),
+      "password": event.otpNumber.toString(),
+    };
+   // print('"body'+body.toString());
+
+    loginModel =
+    await Repository().login(url: '/user/login', data: body);
+    if (loginModel.status == true) {
+      await TempStorage.addToken(loginModel.token.toString());
+
+
+      emit(OtpChecked(role: loginModel.data!.role!));
+    } else {
+      print(loginModel.msg);
+      emit(OtpError(error: loginModel.msg.toString()));
+    }
+  }
+}
+//events
+class AuthEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+
+
+class CheckOTP extends AuthEvent {
+  final String otpNumber, phone;
+  CheckOTP({required this.otpNumber, required this.phone});
+}
+
+
+//states
+
+class AuthState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class CheckingOtp extends AuthState {}
+class OtpChecked extends AuthState {
+  final String role;
+  OtpChecked({required this.role});
+}
+
+class OtpError extends AuthState {
+  final String error;
+  OtpError({required this.error});
+}
